22bca9eac20e46cc3d00729c811cb7233405de15
Mewael Werede <124096999+Mewaeltoti@users.noreply.github.com>
2026-03-13 09:03:36 +0000

    x1

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,32 +1,9 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'app.lovable.b5776ddb68cf4be8ab3a7e7326790308',
-  appName: 'Bingo Ethio',
-  webDir: 'dist',
-  server: {
-    url: 'https://b5776ddb-68cf-4be8-ab3a-7e7326790308.lovableproject.com?forceHideBadge=true',
-    cleartext: true,
-  },
-  plugins: {
-    SplashScreen: {
-      launchShowDuration: 2000,
-      launchAutoHide: true,
-      backgroundColor: '#1a1a2e',
-      androidSplashResourceName: 'splash',
-      androidScaleType: 'CENTER_CROP',
-      showSpinner: false,
-      splashFullScreen: true,
-      splashImmersive: true,
-    },
-  },
-  ios: {
-    contentInset: 'automatic',
-    scheme: 'Bingo Ethio',
-  },
-  android: {
-    backgroundColor: '#1a1a2e',
-  },
+  appId: 'com.yourname.bingo',
+  appName: 'bingo',
+  webDir: 'dist'
 };
 
 export default config;
